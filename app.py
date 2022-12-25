71463f86b54bc01ad31f7f2a9b3831161e805928
caiomolinaro <cmolinaro19@gmail.com>
2022-12-25 16:09:26 -0300

    First Commit

--- app.py
+++ app.py
@@ -1,1 +1,43 @@
-print("Hello World")
+## PLAYGROUND EM PYTHON PARA TESTAR CÓDIGOS
+
+# HELLO WORLD
+
+#print('Hello World')
+
+# FUNÇÃO
+
+'''
+def fun ():
+    print("Função")
+fun()
+'''
+
+# CALCULADORA
+
+def calculadora():
+    num1 = int(input('Digite o primeiro numero: '))
+    op = input("Digite a operação(+ , - , *, /): ")
+    num2 = int(input('Digite o segundo numero: '))
+    
+    if op == '+':
+        res = num1 + num2
+        print("O resultado é ",res)
+
+    elif op == '-':
+        res = num1 - num2
+        print("O resultado é ",res)
+
+    elif op == '*':
+        res = num1 * num2
+        print("O resultado é ",res)
+
+    elif op == '/':
+        res = num1 / num2
+        restoDiv = num1 % num2
+        print("O resultado é ",res, "E o resto da divisão é:", restoDiv)
+
+    else :
+        print("Você digitou algo errado")
+
+
+calculadora()
