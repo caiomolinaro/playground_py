1b4b0ee145567abfd1873f7db548834b8e50eac1
caiomolinaro <cmolinaro19@gmail.com>
2022-12-26 11:56:54 -0300

    Adicionando calculadora de IMC

--- calculadoradeimc.py
+++ calculadoradeimc.py
@@ -1,22 +1,26 @@
-print('Seja bem vindo a calculadora de IMC')
+def calculadoraimc():
 
-nome = input("Digite seu nome: ")
-altura = float(input("Digite sua altura: "))
-peso = float(input("Digite seu peso: "))
+ print('Seja bem vindo a calculadora de IMC')
 
-imc =  (peso / (altura**2))
+ nome = input("Digite seu nome: ")
+ altura = float(input("Digite sua altura: "))
+ peso = float(input("Digite seu peso: "))
 
-if imc < 18.5 :
+ imc =  (peso / (altura**2))
+
+ if imc < 18.5 :
     print(nome, "seu imc é de:", imc, "// você está na classificação de: MAGREZA")
 
-elif imc >= 18.5 and imc <= 24.9 :
+ elif imc >= 18.5 and imc <= 24.9 :
     print(nome, "seu imc é de :", imc, "// você está na classificação: NORMAL")
 
-elif imc >= 25.0 and imc <= 29.9 :
+ elif imc >= 25.0 and imc <= 29.9 :
     print(nome, "seu imc é de :", imc, "// você está na classificação de: SOBREPESO e Obesidade grau I")
 
-elif imc >= 30.0 and imc <= 39.9 :
+ elif imc >= 30.0 and imc <= 39.9 :
     print(nome, "seu imc é de:", imc, "// você está na classificação de: OBESIDADE e Obesidade grau II")
 
-elif imc > 40 :
+ elif imc > 40 :
     print(nome, "seu imc é de:", imc, "// você está na classificação de: OBESIDADE GRAVE e Obesidade grau II")
+
+calculadoraimc()
